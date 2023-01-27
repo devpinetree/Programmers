161b58013983c4063d6761a49403a76c677976ea
DevPineTree <35518716+devpinetree@users.noreply.github.com>
2023-01-27 20:28:47 +0900

    [level 2] Title: 올바른 괄호, Time: 7.04 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -18,26 +18,3 @@
      
      return stack.length === 0;     
  }
-
-
-
-
-
-// function solution(s) {
-//   let bracketArr = [...s];
-//   let stack = [];
-
-//   for (let i = 0; i < bracketArr.length; i++) {
-//     if (stack.length === 0) {
-//       stack.push(bracketArr[i]);
-//     } else {
-//       if (bracketArr[i] === '(') {
-//         stack.push(bracketArr[i]);
-//       } else {
-//         stack.pop();
-//       }
-//     }
-//   }
-
-//   return stack.length === 0;
-// }
